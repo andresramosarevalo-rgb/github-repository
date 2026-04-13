e5b8bbdf91a8ffd9da438b81f6493d6a2668da09
andresramosarevalo-rgb <andresramosarevalo@gmail.com>
2026-04-12 20:22:32 -0500

    first commit

---
+++ src/test/java/org/example/githubrepaso/GithubrepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubrepaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubrepasoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
