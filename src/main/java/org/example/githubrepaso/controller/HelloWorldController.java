fc99b3f8f68f6e779e236ecb0fc99372e6368ac1
andresramosarevalo-rgb <andresramosarevalo@gmail.com>
2026-04-12 21:40:14 -0500

    add: added hello world controller

---
+++ src/main/java/org/example/githubrepaso/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package org.example.githubrepaso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/hello")
+
+public class HelloWorldController {
+
+    @GetMapping("/world")
+    public String helloWorld(){
+
+        return "Hello World";
+    }
+}
